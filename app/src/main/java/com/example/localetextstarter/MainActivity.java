7de0a7cec2eb8049a851ad2bac16f0000a5c14b2
Antonius Wisnu <antonius.kri2003@mail.ugm.ac.id>
2023-03-28 00:10:10 +0700

    Assignment Commit

--- app/src/main/java/com/example/localetextstarter/MainActivity.java
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.localetextstarter;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +8,26 @@ import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText etHarga;
+    TextView tvHasil;
+    Button btnHasil;
+
+    int hasil = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,43 +41,65 @@ public class MainActivity extends AppCompatActivity {
                 showHelp();
             }
         });
-    }
 
-    /**
-     * Shows the Help screen.
-     */
+        Date myDate = new Date();
+        long expirationDate = myDate.getTime() + TimeUnit.DAYS.toMillis(3);
+        myDate.setTime(expirationDate);
+
+        String formatDate = DateFormat.getDateInstance().format(myDate);
+        TextView expiredTextView = findViewById(R.id.date);
+        expiredTextView.setText(formatDate);
+
+        etHarga = findViewById(R.id.etHarga);
+        tvHasil = findViewById(R.id.tvHasil);
+        btnHasil = findViewById(R.id.btnHitung);
+
+        btnHasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try{
+                    String value1 = etHarga.getText().toString();
+                    hasil = (int) Integer.parseInt(value1);
+
+                } catch(NumberFormatException e) {
+                    hasil = (int) 0;
+                }
+
+                int hasil2 = hasil * 100;
+
+                NumberFormat format = NumberFormat.getCurrencyInstance();
+                format.setMaximumFractionDigits(0);
+
+                format.setCurrency(Currency.getInstance(Locale.getDefault()));
+
+                String hasil3 = format.format(hasil2);
+                tvHasil.setText(hasil3);
+
+            }
+        });
+    }
     private void showHelp() {
-        // Create the intent.
         Intent helpIntent = new Intent(this, HelpActivity.class);
-        // Start the HelpActivity.
         startActivity(helpIntent);
     }
 
-    /**
-     * Creates the options menu and returns true.
-     *
-     * @param menu       Options menu
-     * @return boolean   True after creating options menu.
-     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    /**
-     * Handles options menu item clicks.
-     *
-     * @param item      Menu item
-     * @return boolean  True if menu item is selected.
-     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle options menu item clicks here.
         int id = item.getItemId();
         if (id == R.id.action_help) {
             Intent intent = new Intent(this, HelpActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.action_language){
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
             startActivity(intent);
             return true;
         }
